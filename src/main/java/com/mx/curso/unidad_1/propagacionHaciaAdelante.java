7a505ea5a93a14334d9dacd758fb835ea6f4b447
Mahyli <ux24ii402@ux.edu.mx>
2026-03-13 08:58:54 -0600

    ProgramacionHaciaAdelante

---
+++ src/main/java/com/mx/curso/unidad_1/propagacionHaciaAdelante.java
@@ -0,0 +1,33 @@
+package com.mx.curso.unidad_1;
+
+public class propagacionHaciaAdelante (){
+    System.out.println("Realizando propagación hacia adelante...");
+}
+}
+
+class CapaConvolucional extends CapaNeuronal {
+
+    @Override
+    public void propagacionHaciaAdelante() {
+        System.out.println("Transformando tensores con operación convolucional.");
+    }
+}
+
+class CapaDensa extends CapaNeuronal {
+
+    @Override
+    public void propagacionHaciaAdelante() {
+        System.out.println("Transformando tensores con capa densa.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        CapaConvolucional conv = new CapaConvolucional();
+        conv.propagacionHaciaAdelante();
+
+        CapaDensa densa = new CapaDensa();
+        densa.propagacionHaciaAdelante();
+    }
+}
